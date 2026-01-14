3c3d638e5c115f85bb7d6abf470dc6fad35f5f37
DaniOnats <onadan2905@elev.tromsfylke.no>
2026-01-14 15:20:15 +0100

    start with login.php

--- nettside/login.php
+++ nettside/login.php
@@ -1,1 +1,14 @@
+<?php
+session_start();
+include "db.php";
 
+if(isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM data WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+}
+
+?>
