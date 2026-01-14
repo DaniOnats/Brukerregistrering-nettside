007cfefac8ed7246b983fe7f2d0f4a54b3949193
DaniOnats <onadan2905@elev.tromsfylke.no>
2026-01-14 14:26:02 +0100

    Code a html website in index.php

--- nettside/index.php
+++ nettside/index.php
@@ -1,5 +1,44 @@
 <?php
-session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include "db.php";
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brukerregistrering</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Velkommen til Brukerregistrering nettside</h1>
+        <p>Dette er enkelt nettside for å demonstrere Brukerregistrering op påloggning. </p>
+        <p>Vennligst registrer deg eller log inn for å få tilgang til medlemsområdet. </p>
+
+        <div class="skjema">
+            <form action="" method="post">
+                <label for="">Navn:</label><br>
+                <input type="text" id="username" name="username" required><br><br>
+
+                <label for="">E-post:</label><br>
+                <input type="email" id="email" name="email" required><br><br>
+
+                <label for="">Passord:</label><br>
+                <input type="password" id="password" name="password" required><br><br>
+
+                <button type="submit" name="register">Registrer</button>
+            </form>
+        </div>
+
+        <h3> Allerede medlem? <a href="login.php">Logg in her</a></h3>
+
+    </div>
+</body>
+</html>
