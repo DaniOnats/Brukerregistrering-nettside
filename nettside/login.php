d93dca6633a844cffe5edbe6dcc3673035b6fad9
DaniOnats <onadan2905@elev.tromsfylke.no>
2026-01-14 15:31:10 +0100

    login.php php code done

--- nettside/login.php
+++ nettside/login.php
@@ -9,6 +9,21 @@ if(isset($_POST["login"])) {
     $sql = "SELECT * FROM data WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
     $user = mysqli_fetch_assoc($result);
+
+    if ($user) {
+    } else {
+        echo "<p>Ingen bruker med denne e-posten</p>";
+    }
+
+    if ($user && password_verify($password, $user["password"])) {
+        $_SESSION["innloget"] = true;
+        $_SESSION["username"] = $user["username"];
+
+        header("Location: medlem.php");
+        exit();
+    } else {
+        $feil = "Feil e-post eller passord";
+    }
 }
 
 ?>
