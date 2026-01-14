98a522db7030ffc6f4df6c6032f70404eae16e28
DaniOnats <onadan2905@elev.tromsfylke.no>
2026-01-14 13:44:09 +0100

    made db.php and start to create index.php

--- nettside/db.php
+++ nettside/db.php
@@ -1,1 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$database = "brukere";
 
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
