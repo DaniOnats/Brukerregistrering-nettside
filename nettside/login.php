10305a686f7290beccb64683dafff5f87c79a847
DaniOnats <onadan2905@elev.tromsfylke.no>
2026-01-14 15:47:30 +0100

    Finish the login.php page code

--- nettside/login.php
+++ nettside/login.php
@@ -10,20 +10,41 @@ if(isset($_POST["login"])) {
     $result = mysqli_query($conn, $sql);
     $user = mysqli_fetch_assoc($result);
 
-    if ($user) {
+    if (!$user) {
+        $feil = "Feil e-post";
     } else {
-        echo "<p>Ingen bruker med denne e-posten</p>";
-    }
+        if ($user && password_verify($password, $user["password"])) {
+            $feil = "Feil e-post eller passord";
+        } else {
+            $_SESSION["logout"] = true;
+            $_SESSION["username"] = $user["username"];
 
-    if ($user && password_verify($password, $user["password"])) {
-        $_SESSION["innloget"] = true;
-        $_SESSION["username"] = $user["username"];
-
-        header("Location: medlem.php");
-        exit();
-    } else {
-        $feil = "Feil e-post eller passord";
+            header("Location: medlem.php");
+            exit();
+        }        
     }
 }
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Logg inn</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Logg in</h1>
+
+        <?php if (isset($feil)): ?>
+            <p class="feil"><?php echo $feil; ?></p>
+        <?php endif; ?>
+
+        <form action="" method="post">
+            <input type="email" name="email" placeholer="E-post" required>
+            <input type="password" name="password" placeholer="Passord" required>
+            <button type="submit" name="login">Logg inn</button>
+        </form>
+    </div>
+</body>
+</html>
